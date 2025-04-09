25727e0ea319cbacb6730d8f60c983d7a0862a32
Alexandr Kovalchuk <shelter.play.akk@mail.ru>
2025-04-09 20:53:34 +0700

    modified:   src/projects/__pycache__/router.cpython-312.pyc 	modified:   src/tasks/__pycache__/router.cpython-312.pyc 	modified:   src/tasks/router.py

--- src/tasks/router.py
+++ src/tasks/router.py
@@ -2,12 +2,11 @@ from fastapi import APIRouter
 
 from src.tasks.dao import TaskDAO
 from src.tasks.schemas import Task_add
-from src.tasks.models import Task
 
 
 router = APIRouter(prefix='/tasks', tags=['Работа с задачами'])
 
-@router.get('/', summary='Get all tasks', response_model=list(Task))
+@router.get('/', summary='Get all tasks', response_model=list[Task_add])
 async def get_all_tasks():
     return await TaskDAO.get_all()
 
